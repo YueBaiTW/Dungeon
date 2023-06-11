b08ccb597e598a436c3c4654f7e3b1370dcc97a4
jack9325 <134849602+jack9325@users.noreply.github.com>
2023-06-11 16:54:19 +0800

    日期和getexpamount

--- Dungeon/Trigger.h
+++ Dungeon/Trigger.h
@@ -1,4 +1,13 @@
+/*****************************************************************//**
+ * File: Trigger.h
+ * Author: B11115029白安睿 B11115046黃健嘉
+ * Create Date: 2023/??/??
+ * Editor: B11115029白安睿 B11115046黃健嘉
+ * Update Date: 2023/06/11
+ * Description: Create a class named Trigger
+ *********************************************************************/
 #pragma once
+// Include the required library
 #include "Hero.h"
 #include "Position.h"
 #include <string>
@@ -8,22 +17,23 @@ using namespace std;
 class Trigger
 {
 private:
-	Position pos;
-	string icon;
-	int exp;
+	Position pos; // Save its position
+	string icon; // Save its symbol
+	int exp; // Save its exp
 
 public:
-	Trigger();
-	Trigger(int x, int y);
-	Trigger(const Trigger& ref);
+	Trigger(); // Default constructor
+	Trigger(int x, int y); // Give the initial position to build the constructor
+	Trigger(const Trigger& ref); // Give the initial data to build the constructor
 
-	void update(Hero& hero);
-	void setPos(const Position& pos);
-	void setPos(int x, int y);
-	Position getPos() const;
+	void update(Hero& hero); // Update hero's exp
+	void setPos(const Position& pos); // Set the Trigger's position
+	void setPos(int x, int y); // Another method to set the Trigger's position
+	Position getPos() const; // Get the Trigger's position
 
-	// Get Icon
+	// Get Trigger's icon
 	string getIcon() const;
-
+	
+	// Get the total amount of exp obtained by the hero
 	int getExpAmount() const;
 };
