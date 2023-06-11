0453d4f83675bb8d632720ed12a634ca0eda66a8
jack9325 <134849602+jack9325@users.noreply.github.com>
2023-06-11 16:43:13 +0800

    剩日期

--- Dungeon/Rock.h
+++ Dungeon/Rock.h
@@ -1,9 +1,17 @@
+/*****************************************************************//**
+ * File: Rock.h
+ * Author: B11115029白安睿 B11115046黃健嘉
+ * Create Date: 2023/??/??
+ * Editor: B11115029白安睿 B11115046黃健嘉
+ * Update Date: 2023/06/11
+ * Description: Create a class named Rock and inherit to Creature class
+ *********************************************************************/
 #pragma once
 #include "Creature.h"
 
 class Rock:public Creature
 {
 public:
-	Rock();
-	void update(Hero& hero) override;
+	Rock(); //Default constructor
+	void update(Hero& hero) override; //Update the hero's HP
 };
