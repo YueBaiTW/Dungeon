22ea3adcc6689980b5079fc4fde49e1f8344afa5
jack9325 <134849602+jack9325@users.noreply.github.com>
2023-06-11 18:49:41 +0800

    Update Rock.h

--- Dungeon/Rock.h
+++ Dungeon/Rock.h
@@ -4,11 +4,12 @@
  * Create Date: 2023/5/28
  * Editor: B11115029白安睿 B11115046黃健嘉
  * Update Date: 2023/6/11
- * Description: Declare a Rock class inheritance to creature,which has position,icon,
+ * Description: Declare a class named Ghost which inherit from Creature.
  * ***********************************************************************/
 #pragma once
 #include "Creature.h"
 
+//A Rock class which inherit from Creature
 class Rock:public Creature
 {
 public:
